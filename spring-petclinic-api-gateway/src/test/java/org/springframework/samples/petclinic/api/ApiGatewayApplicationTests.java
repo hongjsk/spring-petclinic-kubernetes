ada127b4a2b7ac3e0250b606c354a837200fbf5a
HONGJS <hongjs@kr.ibm.com>
2018-04-20 21:07:20 +0900

    initial

---
+++ spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/ApiGatewayApplicationTests.java
@@ -0,0 +1,18 @@
+package org.springframework.samples.petclinic.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest
+public class ApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
